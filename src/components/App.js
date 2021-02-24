3d83cd57b0793f78306327e294bc95b0a3178ecc
Bleron Limani <bleroni@gmail.com>
2021-02-24 08:55:21 +0100

    Creating the Bootstrap skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,46 @@
 function App() {
   return (
     <div className="App">
-      <div class="jumbotron text-center">
-        <h1>My First Bootstrap Page</h1>
-        <p>Resize this responsive page to see the effect!</p>
-      </div>
-
       <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            <h3>Column 1</h3>
-            <p>Lorem ipsum dolor..</p>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">WouldYouRather App</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">New Question</a></li>
+              <li><a href="#">Leaderboard</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item">
+                <p class="navbar-text welcome-message">Hello, Sarah Edo</p>
+              </li>
+              <li><a href="#">Logout</a></li>
+            </ul>
           </div>
-          <div class="col-sm-4">
-            <h3>Column 2</h3>
-            <p>Lorem ipsum dolor..</p>
-          </div>
-          <div class="col-sm-4">
-            <h3>Column 3</h3>
-            <p>Lorem ipsum dolor..</p>
+        </nav>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <h3>Column 1</h3>
+              <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+              <h3>Column 2</h3>
+              <p>Lorem ipsum dolor..</p>
+            </div>
+            <div class="col-sm-4">
+              <h3>Column 3</h3>
+              <p>Lorem ipsum dolor..</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
