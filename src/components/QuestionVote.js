2919a03a754d7b9a52c8e410f97272530824b433
Bleron Limani <bleroni@gmail.com>
2021-02-25 19:24:46 +0100

    fix: Fixed Radio button functionality

--- src/components/QuestionVote.js
+++ src/components/QuestionVote.js
@@ -3,7 +3,11 @@ import '../styles/QuestionVote.css'
 
 class QuestionVote extends Component {
     handleOnChange = (e) => {
-        alert('handling change...')
+        alert('handling change...' + e.target.value)
+    }
+
+    handleSubmit = () => {
+        alert('handling on submit')
     }
     render() {
         return (
@@ -17,20 +21,20 @@ class QuestionVote extends Component {
                         <hr />
                         <form>
                             <div className="form-check" style={{ display: 'block' }}>
-                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked onChange={this.handleOnChange} />
+                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" onChange={this.handleOnChange} />
                                 <label className="form-check-label" htmlFor="exampleRadios1">
                                     be a front-end developer
                         </label>
                             </div>
                             <div className="form-check" style={{ display: 'block' }}>
-                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" checked onChange={this.handleOnChange} />
+                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" onChange={this.handleOnChange} />
                                 <label className="form-check-label" htmlFor="exampleRadios2">
                                     be a back-end developer
                         </label>
                             </div>
                         </form>
                         <hr />
-                        <button className="btn btn-primary btn-block">Submit</button>
+                        <button className="btn btn-primary btn-block" onClick={this.handleSubmit}>Submit</button>
                         <hr />
                     </div>
                 </div>
