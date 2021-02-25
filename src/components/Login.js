6ac1b0621420b4f3ef722baf2e550b50bc0d1600
Bleron Limani <bleroni@gmail.com>
2021-02-25 19:14:57 +0100

    fix: Tidying Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import Select from 'react-select';
 
 const selectOptions = [
+
     { value: 'sarahedo', label: 'Sarah Edo' },
     { value: 'tylermcginnis', label: 'Tyler McGinnis' },
     { value: 'johndoe', label: 'John Doe' }
 ];
 
 class Login extends Component {
+    handleChange = (e) => {
+        alert(e.value);
+    }
     render() {
         return (
             <div className="new-question-container">
@@ -20,6 +24,8 @@ class Login extends Component {
                         <Select
                             width="200px"
                             options={selectOptions}
+                            onChange={this.handleChange}
+                            placeholder="Select username..."
                         />
                         <hr />
                         <button className="btn btn-primary">Submit</button>
