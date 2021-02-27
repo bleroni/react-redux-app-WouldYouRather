89e88bcc62f6d7d249ad4807a2ed5a6d1ca863bd
Bleron Limani <bleroni@gmail.com>
2021-02-27 14:10:24 +0100

    feat: Sorting the stats

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -5,7 +5,7 @@ import LeaderBoardItem from '../components/LeaderBoardItem'
 class LeaderBoard extends Component {
     render() {
         const { questions } = this.props;
-        let stats = {}
+        let stats = []
 
         Object.keys(this.props.users).map(userId => {
             const totalAnswers = questions.filter((question) => question.optionOne.votes.includes(userId) || question.optionTwo.votes.includes(userId))
@@ -13,9 +13,13 @@ class LeaderBoard extends Component {
             const totalQuestions = questions.filter((question) => question.author === userId)
             const totalScore = totalAnswers.length + totalQuestions.length
             const finalObj = { username: userId, answers: totalAnswers.length, questions: totalQuestions.length, totalScore }
-            console.log(finalObj)
-            return totalAnswers
+            stats.push(finalObj);
+            return ''
         })
+
+        const sortedStats = stats.sort((a, b) => b.totalScore - a.totalScore)
+
+        console.log(sortedStats)
 
         return (
             <div>
