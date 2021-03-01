e6ce62d9d038b51a18fead8a1e1dacb645e2d5d5
Bleron Limani <bleroni@gmail.com>
2021-03-01 09:32:49 +0100

    feat: Started implemented Answered and UnAnswered functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,21 +5,53 @@ import QuestionsListItem from '../components/QuestionsListItem'
 
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showQuestions: 'unanswered'
+        }
+        // this.showUnAnsweredQuestions = this.showUnAnsweredQuestions.bind(this);
+        // this.showAnsweredQuestions = this.showAnsweredQuestions.bind(this);
+    }
+
     componentDidMount() {
         this.props.dispatch(getQuestions());
     }
+    setToTrue = () => {
+        this.setState({ showQuestions: "unanswered" })
+    }
+
+    setToUnTrue = () => {
+        this.setState({ showQuestions: "answered" })
+    }
+
 
     render() {
-        const { questions, users } = this.props;
+        const { questions, users, auth } = this.props;
+        console.log('Starting render AAAAAAAAAAA: ' + JSON.stringify(questions, null, 2))
+        const unansweredQuestions = Object.keys(questions).filter((key) => !questions[key].optionOne.votes.includes(auth.username) && !questions[key].optionTwo.votes.includes(auth.username))
+        const answeredQuestions = Object.keys(questions).filter((key) => questions[key].optionOne.votes.includes(auth.username) || questions[key].optionTwo.votes.includes(auth.username))
+        const displayedQuestions = this.state.showQuestions === 'unanswered' ? unansweredQuestions : answeredQuestions
+
         return (
             <div className="questions-list">
-                <div className="button-container">
-                    <button className="btn btn-half">Unanswered Questions</button>
-                    <button className="btn btn-half-selected">Answered Questions</button>
 
-                    {questions.map((question) => {
-                        return <QuestionsListItem question={question} authorName={users[question.author].name} key={question.id} />
+
+                <div className="button-container">
+                    {this.state.showQuestions === 'unanswered'
+                        ? <button className="btn btn-half-selected" onClick={this.setToTrue}>Unanswered Questions</button>
+                        : <button className="btn btn-half" onClick={this.setToTrue}>Unanswered Questions</button>
+                    }
+
+                    {this.state.showQuestions === 'answered'
+                        ? <button className="btn btn-half-selected" onClick={this.setToUnTrue}>Answered Questions</button>
+                        : <button className="btn btn-half" onClick={this.setToUnTrue}>Answered Questions</button>
+                    }
+
+                    {displayedQuestions.map((key) => {
+                        return <QuestionsListItem question={questions[key]} authorName={users[questions[key].author].name} key={questions[key].id} />
                     })}
+
 
                 </div>
             </div>
@@ -27,10 +59,11 @@ class Home extends Component {
     }
 }
 
-function mapStateToProps({ questions, users }) {
+function mapStateToProps({ questions, users, auth }) {
     return {
         questions,
-        users
+        users,
+        auth
     }
 }
 
