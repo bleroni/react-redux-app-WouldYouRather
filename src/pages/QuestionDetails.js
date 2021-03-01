d66fa0895df8c2d7f389a94595e84a767d18ddf3
Bleron Limani <bleroni@gmail.com>
2021-03-01 10:40:30 +0100

    feat: Implemented redirection to /404 when content not found

--- src/pages/QuestionDetails.js
+++ src/pages/QuestionDetails.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react'
+import { withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
+//import QuestionNew from './QuestionNew';
+import QuestionView from '../components/QuestionView'
+import QuestionVote from '../components/QuestionVote'
 
 class QuestionDetails extends Component {
+    state = {
+        question: {},
+        allVotes: [],
+        userHasVoted: false,
+    }
+    componentDidMount() {
+        const { questions } = this.props
+        const questionId = this.props.match.params.id;
+        const questionArray = questions.filter(question => question.id === questionId)
+        const question = questionArray[0]
+
+        if (question) {
+            const optionOneVotes = question.optionOne.votes;
+            const optionTwoVotes = question.optionTwo.votes;
+            const allVotes = optionOneVotes.concat(optionTwoVotes)
+            this.setState({
+                question,
+                allVotes,
+                hasVoted: allVotes.includes(this.props.auth.username) ? true : false
+            })
+        }
+
+
+
+
+
+        // console.log('Index:' + index);
+
+
+    }
+
     render() {
+        const { questions } = this.props
+        const questionId = this.props.match.params.id;
+        const index = questions.findIndex(x => x.id === questionId);
+        const questionArray = questions.filter(question => question.id === questionId)
+        const question = questionArray[0]
+
+        if (!question) {
+            this.props.history.push('/404')
+
+        }
+
         return (
             <div>
-                a) If question is not answered by user, show: QuestionVote
-                <br />
-                b) Else, show: QuestionView
+
+                {this.state.hasVoted ?
+                    <QuestionView questionId={this.props.match.params.id} question={this.state.question} index={index} />
+                    : <QuestionVote questionId={this.props.match.params.id} question={this.state.question} index={index} />
+                }
+
             </div>
 
         )
     }
 }
 
+function mapStateToProps({ questions, auth }) {
+    return {
+        questions,
+        auth
+    }
+}
 
-export default QuestionDetails;
+
+export default withRouter(connect(mapStateToProps)(QuestionDetails));
